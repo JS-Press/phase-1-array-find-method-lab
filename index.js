4a9324ec2cf8faebd55311df69d8c4b679b64a01
Jarad Solomon <jaradsolomon@gmail.com>
2021-05-30 01:59:32 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin (records){
+const superbowlWon = records.find(record => record.result === "W")
+    if (superbowlWon) {
+        return superbowlWon.year;
+    } else {
+        return undefined;
+    }
+}
